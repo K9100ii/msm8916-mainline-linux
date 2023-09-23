2194ba5e08807e72a3f3c115294edd78cb5249bd
K9100ii <k9100ii.ki@gmail.com>
2023-09-23 18:38:01 +0100

    Input: cyttsp4 - Import forward ported downstream driver
    This is a minimized import of the cyttsp4 touchscreen driver from
    Samsung's downstream G357FZXXU1AOE1 heatqlte kernel, with forward
    porting to get it compiling on mainline Linux, mainly from simply going
    through the git history and applying changes previously made for the
    original mainline cyttsp4 driver, and with changes to clean it up in
    general and remove some unneeded functionality.
    
    The mainline driver was added by Cypress all the way back in Linux v3.11
    and then unfortunately shortly after abandoned. It's missing support for
    device tree configuration, and maybe other basic things, and is
    apparently buggy. This imported downstream version of the driver is more
    mature, but also in a not too great messy state typical for a downstream
    driver.
    
    Seeing that the work had been done on maturing the driver, surely
    Cypress could've cleaned up and sent more patches upstream to bring the
    mainline version into a good state. But unfortunately I guess that never
    happened for whatever reason, and won't.
    
    Meanwhile, it would be a real challenge for someone to improve the
    existing mainline driver, and take things from a downstream version.
    Perhaps, in all likelihood, that will all never happen, and so using
    this downstream driver it shall be.
    
    Thankfully, for long term usage if need be, applying changes to keep the
    driver compiling and working shouldn't be a problem, since pretty much
    just applying changes from commits applied to the mainline driver was
    sufficient to get it compiling, and then working without issues.
    
    Small issue that could be resolved - Figure out how this is occurring:
    [    2.341564] cyttsp4_i2c_adapter 4-0024: Runtime PM usage count underflow!
    
    Also, all parts of the driver are compiled into a single module, even
    though there is meant to be more separation between the parts. But they
    don't compile into individual modules successfully, the downstream
    kernel has them setup as individual objects which would similarly also
    cause problems if compiling the driver as a module there, and getting
    them to be separate looks tricky and not worthwhile for a downstream
    driver.
    
    Signed-off-by: K9100ii <k9100ii.ki@gmail.com>

--- include/linux/platform_data/cyttsp4.h
+++ include/linux/platform_data/cyttsp4.h
@@ -1,36 +1,61 @@
-/* SPDX-License-Identifier: GPL-2.0-only */
+// SPDX-License-Identifier: GPL-2.0-only
 /*
- * Header file for:
- * Cypress TrueTouch(TM) Standard Product (TTSP) touchscreen drivers.
- * For use with Cypress Txx3xx parts.
+ * cyttsp4_core.h
+ * Cypress TrueTouch(TM) Standard Product V4 Core driver module.
+ * For use with Cypress Txx4xx parts.
  * Supported parts include:
- * CY8CTST341
- * CY8CTMA340
+ * TMA4XX
+ * TMA1036
  *
- * Copyright (C) 2009, 2010, 2011 Cypress Semiconductor, Inc.
- * Copyright (C) 2012 Javier Martinez Canillas <javier@dowhile0.org>
+ * Copyright (C) 2012 Cypress Semiconductor
+ * Copyright (C) 2011 Sony Ericsson Mobile Communications AB.
  *
- * Contact Cypress Semiconductor at www.cypress.com (kev@cypress.com)
+ * Author: Aleksej Makarov <aleksej.makarov@sonyericsson.com>
+ * Modifed by: Cypress Semiconductor to add touch settings
+ *
+ * Contact Cypress Semiconductor at www.cypress.com <ttdrivers@cypress.com>
  */
+
 #ifndef _CYTTSP4_H_
 #define _CYTTSP4_H_
 
+#include <linux/stringify.h>
+
+#define CYTTSP4_CORE_NAME "cyttsp4_core"
 #define CYTTSP4_MT_NAME "cyttsp4_mt"
 #define CYTTSP4_I2C_NAME "cyttsp4_i2c_adapter"
 #define CYTTSP4_SPI_NAME "cyttsp4_spi_adapter"
 
-#define CY_TOUCH_SETTINGS_MAX 32
+#define CY_DRIVER_NAME TTDA
+#define CY_DRIVER_MAJOR 02
+#define CY_DRIVER_MINOR 04
 
-struct touch_framework {
-	const uint16_t  *abs;
-	uint8_t         size;
-	uint8_t         enable_vkeys;
-} __packed;
+#define CY_DRIVER_REVCTRL 600162
 
-struct cyttsp4_mt_platform_data {
-	struct touch_framework *frmwrk;
-	unsigned short flags;
-	char const *inp_dev_name;
+#define CY_DRIVER_VERSION		    \
+__stringify(CY_DRIVER_NAME)		    \
+"." __stringify(CY_DRIVER_MAJOR)	    \
+"." __stringify(CY_DRIVER_MINOR)	    \
+"." __stringify(CY_DRIVER_REVCTRL)
+
+#define CY_DRIVER_DATE "20140218"	/* YYYYMMDD */
+
+/* abs settings */
+#define CY_IGNORE_VALUE             0xFFFF
+
+enum cyttsp4_core_platform_flags {
+	CY_CORE_FLAG_NONE		= 0,
+	CY_CORE_FLAG_WAKE_ON_GESTURE	= (1 << 0),
+	CY_CORE_FLAG_POWEROFF_ON_SLEEP	= (1 << 1),
+	/* choose SCAN_TYPE or TOUCH_MODE RAM ID to alter scan type */
+	CY_CORE_FLAG_SCAN_MODE_USES_RAM_ID_SCAN_TYPE = (1 << 2),
+};
+
+enum cyttsp4_loader_platform_flags {
+	CY_LOADER_FLAG_NONE,
+	CY_LOADER_FLAG_CALIBRATE_AFTER_FW_UPGRADE,
+	/* Use CONFIG_VER field in TT_CFG to decide TT_CFG update */
+	CY_LOADER_FLAG_CHECK_TTCONFIG_VERSION,
 };
 
 struct touch_settings {
@@ -39,24 +64,100 @@ struct touch_settings {
 	uint8_t tag;
 } __packed;
 
+struct cyttsp4_touch_firmware {
+	const uint8_t *img;
+	u32 size;
+	const uint8_t *ver;
+	u8 vsize;
+	u8 hw_version;
+	u16 fw_version;
+	u8 cfg_version;
+} __packed;
+
+struct cyttsp4_touch_config {
+	struct touch_settings *param_regs;
+	struct touch_settings *param_size;
+	const uint8_t *fw_ver;
+	uint8_t fw_vsize;
+};
+
+struct cyttsp4_loader_platform_data {
+	struct cyttsp4_touch_firmware *fw;
+	struct cyttsp4_touch_config *ttconfig;
+	const char *sdcard_path;
+	u32 flags;
+} __packed;
+
+typedef int (*cyttsp4_platform_read) (struct device *dev, u16 addr,
+	void *buf, int size);
+
+#define CY_TOUCH_SETTINGS_MAX 32
+
 struct cyttsp4_core_platform_data {
 	int irq_gpio;
 	int rst_gpio;
 	int level_irq_udelay;
+	int max_xfer_len;
 	int (*xres)(struct cyttsp4_core_platform_data *pdata,
 		struct device *dev);
 	int (*init)(struct cyttsp4_core_platform_data *pdata,
 		int on, struct device *dev);
 	int (*power)(struct cyttsp4_core_platform_data *pdata,
 		int on, struct device *dev, atomic_t *ignore_irq);
+	int (*detect)(struct cyttsp4_core_platform_data *pdata,
+		struct device *dev, cyttsp4_platform_read read);
 	int (*irq_stat)(struct cyttsp4_core_platform_data *pdata,
 		struct device *dev);
 	struct touch_settings *sett[CY_TOUCH_SETTINGS_MAX];
+	struct cyttsp4_loader_platform_data *loader_pdata;
+	u32 flags;
+	u8 easy_wakeup_gesture;
+	int avdd_gpio;
+	int vddo_gpio;
+	struct pinctrl *ts_pinctrl;
+	struct pinctrl_state *gpio_state_active;
+	struct pinctrl_state *gpio_state_suspend;
+};
+
+struct touch_framework {
+	const uint16_t  *abs;
+	uint8_t         size;
+	uint8_t         enable_vkeys;
+} __packed;
+
+enum cyttsp4_mt_platform_flags {
+	CY_MT_FLAG_NONE = 0x00,
+	CY_MT_FLAG_HOVER = 0x04,
+	CY_MT_FLAG_FLIP = 0x08,
+	CY_MT_FLAG_INV_X = 0x10,
+	CY_MT_FLAG_INV_Y = 0x20,
+	CY_MT_FLAG_VKEYS = 0x40,
+	CY_MT_FLAG_NO_TOUCH_ON_LO = 0x80,
+};
+
+struct cyttsp4_mt_platform_data {
+	struct touch_framework *frmwrk;
+	unsigned short flags;
+	char const *inp_dev_name;
+	int vkeys_x;
+	int vkeys_y;
 };
 
 struct cyttsp4_platform_data {
 	struct cyttsp4_core_platform_data *core_pdata;
 	struct cyttsp4_mt_platform_data *mt_pdata;
+	struct cyttsp4_loader_platform_data *loader_pdata;
 };
+
+extern struct cyttsp4_loader_platform_data _cyttsp4_loader_platform_data;
+
+int cyttsp4_xres(struct cyttsp4_core_platform_data *pdata,
+		struct device *dev);
+int cyttsp4_init(struct cyttsp4_core_platform_data *pdata,
+		int on, struct device *dev);
+int cyttsp4_power(struct cyttsp4_core_platform_data *pdata,
+		int on, struct device *dev, atomic_t *ignore_irq);
+int cyttsp4_irq_stat(struct cyttsp4_core_platform_data *pdata,
+		struct device *dev);
 
 #endif /* _CYTTSP4_H_ */
